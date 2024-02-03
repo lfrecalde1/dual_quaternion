a80f317f773d5d020d66cf1036ce04f57a11760c
fer <fernandorecalde@uti.edu.ec>
2024-02-03 18:14:04 -0500

    Quaternion Class

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,33 @@
-from dual_quaternion.quaternion import name
+#!/usr/bin/env python3
+import time
+import rospy
+import numpy as np
 
-name()
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dual_quaternion.quaternion import Quaternion
+
+def main():
+    # Init Quaternion Axis angle
+    theta = 3.8134
+    n = np.array([0.4896, 0.2032, 0.8480])
+    q0 = np.hstack([np.cos(theta / 2), np.sin(theta / 2) * np.array(n)])
+
+    # Object quaternion
+    q = Quaternion(q = q0)
+    aux = q.__repr__()
+
+    # Check elements
+    print(aux)
+
+    return None
+if __name__ == '__main__':
+    try:
+        main()
+    except(rospy.ROSInterruptException, KeyboardInterrupt):
+        print("Error System")
+        pass
+    else:
+        print("Complete Execution")
+        pass
--- src/dual_quaternion/quaternion.py
+++ src/dual_quaternion/quaternion.py
@@ -1,3 +1,38 @@
-def name():
-    print("Fer")
-    return None
+from numbers import Number
+import numpy as np
+import random
+
+
+class Quaternion():
+    def __init__(self, qw=0.0, qx=0.0, qy=0.0, qz=0.0, q=None):
+        # Class initialization
+        # INPUT:
+        #  qw: Scalar part of the quaternion.
+        #  qx: First element of the vector part of the quaternion.
+        #  qy: Second element of the vector part of the quaternion.
+        #  qz: Third element of the vector part of the quaternion.
+        #  q: Quaternion as an array, list, tuple, or numpy array [qw, qx, qy, qz].
+        # Example:
+        #  q = Quaternion(q=[qw, qx, qy, qz])
+        #  q = Quaternion(q=(qw, qx, qy, qz))
+        #  q = Quaternion(q=np.array([qw, qx, qy, qz]))
+        #  q = Quaternion(qw, qx, qy, qz)
+        if q is not None:
+            if isinstance(q, (np.ndarray, list, tuple)):
+                if len(q) != 4:
+                    raise ValueError("Array q must have exactly 4 elements.")
+                self.q = np.array(q, dtype=np.double)
+            else:
+                raise TypeError("q must be an ndarray, list, or tuple.")
+        else:
+            if not all(isinstance(i, Number) for i in [qw, qx, qy, qz]):
+                raise TypeError("qw, qx, qy, qz should be scalars.")
+            self.q = np.array([qw, qx, qy, qz], dtype=np.double)
+
+    def __str__(self):
+        return np.str(self.q)
+
+    def __repr__(self):
+        return "<Quaternion w:{} x:{} y:{} z:{}>".format(self.q[0], self.q[1], self.q[2], self.q[3])
+
+
