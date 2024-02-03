679325c3f357baf4f04b75d0786711a77383cd2d
fer <fernandorecalde@uti.edu.ec>
2024-02-03 15:52:41 -0500

    install

--- install_python.sh
+++ install_python.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Installing Python libraries..."
-pip install numpy==1.21.2
+pip install numpy==1.24.4
 pip install scipy==1.7.2
 pip install sympy
 pip install matplotlib
-pip install casadi==3.5.1
+pip install casadi==3.6.3
 pip install pluggy==1.3.0
 pip install pyyaml
 pip install -U rospkg
