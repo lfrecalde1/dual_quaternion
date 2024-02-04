f4dfe57fc2f3e951354860f004ba48800d35a310
fer <fernandorecalde@uti.edu.ec>
2024-02-03 19:22:00 -0500

    rviz quat

--- scripts/main.py
+++ scripts/main.py
@@ -2,28 +2,56 @@
 import time
 import rospy
 import numpy as np
-
+import casadi as ca
 import numpy as np
 import matplotlib.pyplot as plt
-
 from dual_quaternion.quaternion import Quaternion
 
 def main():
+    # Sample Time Defintion
+    sample_time = 0.05
+
+    # Time defintion aux variable
+    t = 0
+
+    # Frequency of the simulation
+    hz = int(1/sample_time)
+    loop_rate = rospy.Rate(hz)
+
+    # Message Ros
+    rospy.loginfo_once("Quaternion.....")
+
     # Init Quaternion Axis angle
-    theta = 3.8134
-    n = np.array([0.4896, 0.2032, 0.8480])
+    theta = np.pi/4
+    n = np.array([0.0, 0.0, 1.0])
     q0 = np.hstack([np.cos(theta / 2), np.sin(theta / 2) * np.array(n)])
 
     # Object quaternion
     q = Quaternion(q = q0)
-    aux = q.__repr__()
 
-    # Check elements
-    print(aux)
+    # Message 
+    message_ros = "Quaternion "
+    while not rospy.is_shutdown():
+        tic = rospy.get_time()
 
+
+        # Time restriction Correct
+        loop_rate.sleep()
+
+        # Send Odometry
+        q.send_odometry()
+
+        # Print Time Verification
+        toc = rospy.get_time()
+        delta = toc - tic
+        rospy.loginfo(message_ros + str(delta))
+        t = t + delta
     return None
+
 if __name__ == '__main__':
     try:
+        # Initialization Node
+        rospy.init_node("Quaternions",disable_signals=True, anonymous=True)
         main()
     except(rospy.ROSInterruptException, KeyboardInterrupt):
         print("Error System")
--- src/dual_quaternion/quaternion.py
+++ src/dual_quaternion/quaternion.py
@@ -1,10 +1,12 @@
 from numbers import Number
 import numpy as np
+import rospy
+from nav_msgs.msg import Odometry
 import random
 
 
 class Quaternion():
-    def __init__(self, qw=0.0, qx=0.0, qy=0.0, qz=0.0, q=None):
+    def __init__(self, qw=0.0, qx=0.0, qy=0.0, qz=0.0, q=None, name = "quat"):
         # Class initialization
         # INPUT:
         #  qw: Scalar part of the quaternion.
@@ -28,6 +30,15 @@ class Quaternion():
             if not all(isinstance(i, Number) for i in [qw, qx, qy, qz]):
                 raise TypeError("qw, qx, qy, qz should be scalars.")
             self.q = np.array([qw, qx, qy, qz], dtype=np.double)
+        # Odometry message
+        self.odom_msg = Odometry()
+
+        # Name
+        self.name = name
+
+        # Publisher Odometry
+        odomety_topic = "/" + self.name + "/odom"
+        self.odometry_publisher = rospy.Publisher(odomety_topic, Odometry, queue_size = 10)
 
     def __str__(self):
         return np.str(self.q)
@@ -35,4 +46,25 @@ class Quaternion():
     def __repr__(self):
         return "<Quaternion w:{} x:{} y:{} z:{}>".format(self.q[0], self.q[1], self.q[2], self.q[3])
 
+    def get_odometry(self):
+        # Function to send the Oritentation of the Quaternion
+        self.odom_msg.header.stamp = rospy.Time.now()
+        self.odom_msg.header.frame_id = "map"
+        self.odom_msg.child_frame_id = self.name
 
+        self.odom_msg.pose.pose.position.x = 0
+        self.odom_msg.pose.pose.position.y = 0
+        self.odom_msg.pose.pose.position.z = 0
+
+        self.odom_msg.pose.pose.orientation.x = self.q[1]
+        self.odom_msg.pose.pose.orientation.y = self.q[2]
+        self.odom_msg.pose.pose.orientation.z = self.q[3]
+        self.odom_msg.pose.pose.orientation.w = self.q[0]
+        return None
+
+    def send_odometry(self):
+        # Function to send the orientation of the Quaternion
+        self.get_odometry()
+        # Send Odometry
+        self.odometry_publisher.publish(self.odom_msg)
+        return None
